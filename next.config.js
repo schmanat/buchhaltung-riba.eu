c7c72c81b5c62e8b8c8ee7b472036c60033dd698
schman <manuel.schneider@schman.at>
2026-01-02 08:15:36 +0100

    downgrade to React 18

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+
+  // Für statisches Hosting i.d.R. sinnvoll (verhindert 404s je nach Webserver-Setup)
+  trailingSlash: true,
+
+  // Next/Image ohne Server-Optimizer betreiben
+  images: {
+    unoptimized: true,
+  },
+
+  // Kleinigkeiten
+  reactStrictMode: true,
+  poweredByHeader: false,
+  compress: true,
+};
 
 module.exports = nextConfig;
